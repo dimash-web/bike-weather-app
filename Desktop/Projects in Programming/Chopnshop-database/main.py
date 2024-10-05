35f5669d091ea580947024fcf35ef6ec4d294e34
Dinmukhamed Shubay <ds6704@nyu.edu>
2024-10-05 19:20:46 -0400

    Database setup

--- Desktop/Projects in Programming/Chopnshop-database/main.py
+++ Desktop/Projects in Programming/Chopnshop-database/main.py
@@ -1,15 +1,17 @@
-from dotenv import load_dotenv
 import os
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import pymongo
 
 load_dotenv()
 
-uri = os.getenv("MONGO_URI")
-client = MongoClient(uri, server_api=ServerApi('1'))
+mongodb_uri = os.getenv("MONGO_URI")
+
+client = pymongo.MongoClient(mongodb_uri)
 
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+client.close()
